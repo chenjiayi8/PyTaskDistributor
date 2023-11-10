938f7bd7d6dd0c5d0a5161cb270b5af065ee6f13
Jiayi Chen <chenjiayi_344@hotmail.com>
2023-11-10 16:49:31 +0100

    Refactor task progress checking in Monitor class

--- util/monitor.py
+++ util/monitor.py
@@ -77,37 +77,34 @@ class Monitor:
         msg += self.print_server_progress()
         return msg
 
-    def print_task_progress(self):
-        if self.master.fast_mode:
-            msg = "Task (Fast mode):\n"
-        else:
-            msg = "Task:\n"
+    def check_task_progress(self, tasks: dict, servers: list):
+        """Check the progress of the task."""
+        msg = "Task:\n"
         df_tasks = pd.DataFrame(columns=self.columns_task)
-        task_list = self.master.get_task_list()
         assigned_sessions = []
         finished_sessions = []
         current_sessions = []
-        for server in self.master.server_list:
+        for server in servers:
             assigned_sessions += server["assigned_sessions"]
             finished_sessions += server["finished_sessions"].keys()
             current_sessions += server["current_sessions"].keys()
-        for task in task_list:
-            time_str = get_time_str(task)
-            task_path = os.path.join(self.master.new_task_folder, task)
-            df = read_json_to_df(task_path)
-            df = df.sort_values("Num")
-            df["UUID"] = df["UUID"].apply(str)
-            temp = list(zip(["Task"] * len(df), df["Num"].apply(str), df["UUID"]))
+        for task_name in tasks.keys():
+            df_task, updated_time = tasks[task_name]
+            time_str = get_time_str(task_name)
+            df_task = df_task.sort_values("Num")
+            df_task["UUID"] = df_task["UUID"].apply(str)
+            temp = list(
+                zip(["Task"] * len(df_task), df_task["Num"].apply(str), df_task["UUID"])
+            )
             index1 = ["-".join(t) for t in temp]
             index2 = [time_str + "_" + "-".join(t) for t in temp]
-            num_task = len(df)
+            num_task = len(df_task)
             num_assigned = len(get_duplicated_items(assigned_sessions, index1))
             num_finished = len(get_duplicated_items(finished_sessions, index2))
             num_running = len(get_duplicated_items(current_sessions, index1))
-            updated_time = datetime.fromtimestamp(os.path.getmtime(task_path))
             updated_time_str = parse_time(updated_time)
             data = [
-                task[:-5],
+                task_name,
                 num_task,
                 num_assigned,
                 num_running,
